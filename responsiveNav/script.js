8f45858bd298ac9d3ba4789e2b1d63ad2da861c2
coder9747 <67683689+coder9747@users.noreply.github.com>
2022-12-25 21:42:16 +0530

    Update script.js

--- responsiveNav/script.js
+++ responsiveNav/script.js
@@ -6,3 +6,17 @@ handBurger.addEventListener("click",()=>
     nav.classList.toggle("responsiveNav");
 
 })
+window.addEventListener("scroll",fixnav);
+function fixnav()
+{
+    // console.log("Hello World!");
+    if(window.scrollY>nav.clientHeight)
+    {
+        navBar.classList.add("active");
+    }
+    else
+    {
+        navBar.classList.remove("active");
+    }
+
+}
